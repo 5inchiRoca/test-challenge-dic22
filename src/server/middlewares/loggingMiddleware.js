ebcc41dd8ac5402c276e5e9cdf19bcf81ce1b57d
Johann Santos <jsantost.dev@gmail.com>
2022-12-23 00:13:06 -0500

    feat(getLogs): Actualizar funcion para obtener logs de endpoints

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,9 +1,31 @@
+const { sequelize: { models: { logging } } } = require('../../app/db');
+
 const loggingMiddleware = (db) =>
     (req, res, next) => {
+
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
         const headers = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
         // Persist this info on DB
+
+        const endpoints =['getPeople', 'getPlanet', 'getWeightOnPlanetRandom']
+
+        let persist = false;
+
+        endpoints.forEach(e => {
+            if( req.originalUrl.includes(e) ){
+                persist = true;
+            }
+        })
+
+        if( persist ){
+            logging.create({
+                action: originalUrl,
+                header: headers,
+                ip,
+            })
+        }
+
         next();
     }
 
